19cb77cc1f40bc3d169176f116a5078def83d340
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-07-22 20:47:28 -0500

    Create 05-deses

---
+++ 01-ts-intro/src/exercises/05-deses.ts
@@ -0,0 +1,34 @@
+
+interface Product {
+    desc: string;
+    price: number;
+}
+
+const phone: Product = {
+    desc: 'Beetle Phone',
+    price: 10000
+}
+
+const laptop: Product = {
+    desc: 'Dell Inspiron',
+    price: 20000
+}
+
+function calculateISV( products: Product[] ): [number, number] {
+    
+    let total = 0;
+
+    products.forEach(({ price }) => {
+        total += price;
+    });
+
+    return [total, total * 0.15];
+
+}
+
+const articles = [ phone, laptop ];
+
+const [total, isv] = calculateISV( articles );
+
+console.log( `Total: ${total}` );
+console.log( `ISV: ${ isv }` );
