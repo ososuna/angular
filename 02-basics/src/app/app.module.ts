55c086355b39ded88f2afd916780b3a66267aa1c
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-07-24 14:21:42 -0500

    Init DBZ Module

--- 02-basics/src/app/app.module.ts
+++ 02-basics/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
 import { CounterComponent } from './counter/counter/counter.component';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
@@ -12,8 +13,9 @@ import { HeroesModule } from './heroes/heroes.module';
   ],
   imports: [
     BrowserModule,
-    HeroesModule,
-    CounterModule
+    CounterModule,
+    DbzModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
