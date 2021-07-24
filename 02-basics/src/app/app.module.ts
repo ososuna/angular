b1707e6314fa4058b5d54a232b79c40fc82cb056
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-07-24 13:52:06 -0500

    Create Counter Module

--- 02-basics/src/app/app.module.ts
+++ 02-basics/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterModule } from './counter/counter.module';
+import { CounterComponent } from './counter/counter/counter.component';
 import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
@@ -10,7 +12,8 @@ import { HeroesModule } from './heroes/heroes.module';
   ],
   imports: [
     BrowserModule,
-    HeroesModule
+    HeroesModule,
+    CounterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 02-basics/src/app/counter/counter/counter.component.ts
+++ 02-basics/src/app/counter/counter/counter.component.ts
@@ -2,16 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-counter',
-    template: `
-        <h1>{{ title }}</h1>
-        <h3>The base is: <strong>{{ base }}</strong></h3>
-
-        <button (click)="acumulate( base )">+{{ base }}</button>
-
-        <span>{{ number }}</span>
-
-        <button (click)="acumulate( -base )">{{ - base }}</button>
-    `
+    templateUrl: 'counter.component.html'
 })
 export class CounterComponent {
     
