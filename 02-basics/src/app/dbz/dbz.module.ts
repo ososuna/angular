db950325da72a6fe3617cac78b4ef7807b6ea94c
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-07-24 17:01:09 -0500

    Init Characters Component

--- 02-basics/src/app/dbz/dbz.module.ts
+++ 02-basics/src/app/dbz/dbz.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { MainComponent } from './main/main.component';
+import { CharactersComponent } from './characters/characters.component';
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    CharactersComponent
   ],
   exports: [
     MainComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class DbzModule { }
--- 02-basics/src/app/dbz/main/main.component.ts
+++ 02-basics/src/app/dbz/main/main.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Character {
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  templateUrl: './main.component.html'
 })
-export class MainComponent implements OnInit {
+export class MainComponent {
 
-  constructor() { }
+  characters: Character[] = [
+    {
+      name: 'Goku',
+      power: 15000
+    },
+    {
+      name: 'Vegeta',
+      power: 8500
+    }
+  ];
 
-  ngOnInit(): void {
+  newCharacter: Character = {
+    name: '',
+    power: 0
+  }
+
+  add(): void {
+    if ( this.newCharacter.name.trim().length === 0 ) return;
+  
+    this.characters.push( this.newCharacter );
+
+    this.newCharacter = {
+      name: '',
+      power: 0
+    };
+
   }
 
 }
