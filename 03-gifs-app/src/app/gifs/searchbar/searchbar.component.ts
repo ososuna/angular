aa0fbd7b7e9cb44419645678e7902a52ee325634
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-07-29 18:52:00 -0500

    Update Searchbar Component ViewChild txtSearch

--- 03-gifs-app/src/app/gifs/searchbar/searchbar.component.ts
+++ 03-gifs-app/src/app/gifs/searchbar/searchbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-searchbar',
@@ -8,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchbarComponent implements OnInit {
 
+  @ViewChild('txtSearch') txtSearch!: ElementRef<HTMLInputElement>;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  search(): void {
+    const value = this.txtSearch.nativeElement.value;
+    console.log( value );
+    this.txtSearch.nativeElement.value = '';
   }
 
 }
