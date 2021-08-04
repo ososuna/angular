a96776328e036bebab4925fcf32313b690e7ab8e
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-08-04 01:16:03 -0500

    Update Results Component HttpCLientModule

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,11 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apiKey: string = 'NqFFjmEcN1iM7bF1r2n9xb8kTXq3rk94';
   private _history: string[] = [];
+  public results: any[] = [];
 
   get history(): string[] {
     return [...this._history];
   }
+
+  constructor ( private http: HttpClient ) {}
 
   searchGifs( query: string = '' ) {
     
@@ -19,10 +24,13 @@ export class GifsService {
       this._history.unshift( query );
       this._history = this._history.splice(0, 10);
     }
-    
-    console.log( this._history );
   
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=NqFFjmEcN1iM7bF1r2n9xb8kTXq3rk94&q=${ query }&limit=10`)
+      .subscribe( ( resp: any ) => {
+        console.log( resp.data );
+        this.results = resp.data;
+      });
+
   }
 
-  constructor() { }
 }
