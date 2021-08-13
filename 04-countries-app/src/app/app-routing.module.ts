3119d49085610da4128f133f6ca31bfd399bff74
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-08-12 21:59:41 -0500

    Added RouterLink to sidebar

--- 04-countries-app/src/app/app-routing.module.ts
+++ 04-countries-app/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { ByCapitalComponent } from "./country/pages/by-capital/by-capital.component";
 import { ByCountryComponent } from "./country/pages/by-country/by-country.component";
 import { ByRegionComponent } from './country/pages/by-region/by-region.component';
-import { ShowCountryComponent } from "./country/pages/watch-country/show-country.component";
+import { ShowCountryComponent } from "./country/pages/show-country/show-country.component";
 
 const routes: Routes = [
     {
--- 04-countries-app/src/app/country/country.module.ts
+++ 04-countries-app/src/app/country/country.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ByCapitalComponent } from './pages/by-capital/by-capital.component';
 import { ByCountryComponent } from './pages/by-country/by-country.component';
 import { ByRegionComponent } from './pages/by-region/by-region.component';
-import { ShowCountryComponent } from './pages/watch-country/show-country.component';
+import { ShowCountryComponent } from './pages/show-country/show-country.component';
 
 
 
