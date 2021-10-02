b7645e71cf8ec7454820b4b5095deaad9f2f17a8
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-10-02 00:14:27 -0500

    Update createUser controller and added JWT

--- 13-auth-server/controllers/auth.js
+++ 13-auth-server/controllers/auth.js
@@ -1,13 +1,48 @@
 const { request, response } = require('express');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const { generateJWT } = require('../helpers/jwt');
 
-const createUser = (req = request, res = response) => {
-
+const createUser = async(req = request, res = response) => {
+    
     const { email, name, password } = req.body;
 
-    return res.json({
-        msg: 'Create user'
-    });
+    try {        
+        // Verify email
+        const user = await User.findOne({ email });
 
+        if ( user ) {
+            return res.status(400).json({
+                msg: 'Already existing email'
+            });
+        }
+
+        // Create user model
+        const dbUser = new User( req.body );
+
+        // Hash password
+        const salt = bcrypt.genSaltSync();
+        dbUser.password = bcrypt.hashSync( password, salt );
+
+        // Generate JWT
+        const token = await generateJWT( dbUser.id, name );
+
+        // Create DB user
+        await dbUser.save();
+
+        // Generate successful response
+        return res.status(201).json({
+            uid: dbUser.id,
+            name,
+            email,
+            token
+        });
+
+    } catch ( error ) {
+        return res.status(500).json({
+            msg: 'Please talk to the admin'
+        });
+    }
 }
 
 const loginUser = (req = request, res = respose) => {
