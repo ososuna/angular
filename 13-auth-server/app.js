02f6b3e75a31ff510dab1e66640e31fc872c311f
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-10-01 22:22:32 -0500

    Added env file

--- 13-auth-server/app.js
+++ 13-auth-server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config(); 
 
 // Create express server
 const app = express();
@@ -13,6 +14,6 @@ app.use( express.json() );
 // Routes
 app.use( '/api/auth', require('./routes/auth') );
 
-app.listen( 4000, () => {
-    console.log(`Server running in port ${ 4000 }`);
+app.listen( process.env.PORT, () => {
+    console.log(`Server running in port ${ process.env.PORT }`);
 });
