e7b9815152c560810ad99aa24def0406249252c3
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-10-01 23:23:35 -0500

    Added database connection

--- 13-auth-server/app.js
+++ 13-auth-server/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('./db/config');
 require('dotenv').config(); 
 
 // Create express server
 const app = express();
+
+// Database
+dbConnection();
 
 // CORS
 app.use( cors() );
